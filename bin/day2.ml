b1bb937ff8cb08378cf14ededcd55990d12735b9
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2024-12-02 12:19:19 +0530

    Add day 2 part 2

--- bin/day2.ml
+++ bin/day2.ml
@@ -12,17 +12,43 @@ let rec check_for_same_signs current_sign = function
                     false else if (abs a) > 3 || (abs a) < 1 then false
                   else check_for_same_signs (a / (abs a)) (b::rest)
 
+let evaluate_one_dropped l =
+  let a = Array.of_list l in
+  let found_valid = ref false in
+  let al = (Array.length a) in
+  for i = 0 to (al - 1) do
+    if ([Array.sub a 0 i |> Array.to_list;Array.sub a (i + 1) (al - i - 1) |> Array.to_list]
+        |> List.concat
+        |> successive_diff
+        |> check_for_same_signs 1)
+    then found_valid := true
+  done;
+  !found_valid
+
 let () =
   let input = "inputs/day2.txt" in
-  input
-  |> read_lines
-  |> List.map (String.split_on_char ' ')
-  |> List.map (fun x -> List.filter (fun y -> String.length y > 0) x)
-  |> List.filter (fun x -> List.length x > 1)
-  |> List.map (fun x -> List.map int_of_string x)
-  |> List.map successive_diff
+  let lines =
+    input
+    |> read_lines
+    |> List.map (String.split_on_char ' ')
+    |> List.map (fun x -> List.filter (fun y -> String.length y > 0) x)
+    |> List.filter (fun x -> List.length x > 1)
+  in
+  let differences =
+    lines
+    |> List.map (fun x -> List.map int_of_string x)
+    |> List.map successive_diff
+  in
+  differences
   |> List.map (check_for_same_signs 1)
   |> List.filter (fun x -> x)
   |> List.length
   |> string_of_int
-  |> print_endline
+  |> print_endline;
+  lines
+  |> List.map (fun x -> List.map int_of_string x)
+  |> List.map evaluate_one_dropped
+  |> List.filter (fun x -> x)
+  |> List.length
+  |> string_of_int
+  |> print_endline;
