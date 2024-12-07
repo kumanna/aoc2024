dbc228429eea53616b38c45a55d1f42995b1cc68
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2024-12-07 12:04:54 +0530

    Add day 7 part 2 solution

---
+++ bin/day7.ml
@@ -0,0 +1,48 @@
+open Utils
+
+module OperatorCheck = struct
+
+  let create result int_list =
+    (result, int_list)
+
+  let check_possible oc =
+    let (result, int_list) = oc in
+    let rec generate_possibilities l running_results =
+      match l with
+      | [] -> running_results
+      | [a] ->
+        if running_results = [] then [a]
+        else
+        List.concat [List.map (fun x -> x + a) running_results;
+                     List.map (fun x -> x * a) running_results;
+                     List.map (fun x -> (int_of_string ((string_of_int x) ^ (string_of_int a)))) running_results]
+      | a::b::rest ->
+        if running_results = [] then
+          generate_possibilities rest [a + b;a * b;(int_of_string ((string_of_int a) ^ (string_of_int b)))]
+        else
+          let new_running_list =
+            List.concat [List.map (fun x -> x + a) running_results;
+                         List.map (fun x -> x * a) running_results;
+                         List.map (fun x -> (int_of_string ((string_of_int x) ^ (string_of_int a)))) running_results]
+          in
+          generate_possibilities (b::rest) new_running_list
+    in List.mem result (generate_possibilities int_list [])
+
+  let get_result (r, _) = r
+end
+
+let () =
+  let input_file = "inputs/day7.txt" in
+  input_file
+  |> read_lines
+  |> List.filter (fun x -> String.length x > 1)
+  |> List.map (String.split_on_char ':')
+  |> List.map (fun x -> List.map String.trim x)
+  |> List.filter (fun x -> List.length x = 2)
+  |> List.map (fun x -> ((x |> List.hd |> int_of_string, x |> List.tl |> List.hd |> String.split_on_char ' ' |> List.map int_of_string)))
+  |> List.map (fun (x, y) -> OperatorCheck.create x y)
+  |> List.filter_map (fun x -> if OperatorCheck.check_possible x then (Some x) else None)
+  |> List.map OperatorCheck.get_result
+  |> List.fold_left (fun x y -> x + y) 0
+  |> string_of_int
+  |> print_endline
