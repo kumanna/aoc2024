5e2acdd5bffab97e215981e3399c4803c72e0d86
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2024-12-08 16:52:48 +0530

    Use set instead of lists

--- bin/day8.ml
+++ bin/day8.ml
@@ -2,7 +2,16 @@ open Utils
 
 let explode s = List.init (String.length s) (String.get s)
 
+module Grid_Point = struct
+  type t = int * int
+
+  let compare (a1row, a1col) (a2row, a2col) =
+    if a1row = a2row then a1col - a2col else a1row - a2row
+end
+
 module Char_Set = Set.Make(Char)
+
+module Grid_Point_Set = Set.Make(Grid_Point)
 
 let find_antinode (a1row, a1col) (a2row, a2col) grid =
   let rowdiff = a2row - a1row in
@@ -15,10 +24,12 @@ let find_antinode (a1row, a1col) (a2row, a2col) grid =
     Some (new_row, new_col)
 
 let rec find_antinodes_partb a1 a2 running_list grid =
-  let running_list = if running_list = [] then [a1;a2] else running_list in
+  let running_list = if (running_list |> Grid_Point_Set.cardinal) = 0 then
+      running_list |> Grid_Point_Set.add a1 |> Grid_Point_Set.add a2
+    else running_list in
   let new_node = find_antinode a1 a2 grid in
   match new_node with
-  | Some x -> find_antinodes_partb x a1 (x::running_list) grid
+  | Some x -> find_antinodes_partb x a1 (Grid_Point_Set.add x running_list) grid
   | None -> running_list
 
 let () =
@@ -52,11 +63,10 @@ let () =
   |> print_endline;
   Char_Set.to_list !cs
   |> List.map (Hashtbl.find_all antenna_table)
-  |> List.map (fun x -> List.map (fun y -> List.map (fun z -> find_antinodes_partb z y [] grid) x) x)
+  |> List.map (fun x -> List.map (fun y -> List.map (fun z -> find_antinodes_partb z y Grid_Point_Set.empty grid) x) x)
   |> List.concat
   |> List.concat
-  |> List.concat
-  |> List.sort_uniq compare
-  |> List.length
+  |> List.fold_left (fun x y ->  Grid_Point_Set.union x y) Grid_Point_Set.empty
+  |> Grid_Point_Set.cardinal
   |> string_of_int
   |> print_endline;
