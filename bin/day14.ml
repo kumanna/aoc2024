6da701f9045f1809f373e4379b77ca8fc73cdcc8
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2024-12-14 13:44:19 +0530

    Add day 14 part 1

---
+++ bin/day14.ml
@@ -0,0 +1,75 @@
+open Utils
+
+let n_rows = 103
+
+let n_cols = 101
+
+module Robot = struct
+
+  type t = {
+    position_row : int ;
+    position_col : int ;
+    velocity_row : int ;
+    velocity_col : int ;
+  }
+
+  let create s =
+    let split_string = s |> String.split_on_char ' ' |> List.map (fun x -> String.sub x 2 ((String.length x) - 2)) |> List.map (fun x -> String.split_on_char ',' x) in
+    match split_string with
+    | [[position_col_;position_row_];
+       [velocity_col_;velocity_row_]] ->
+        let position_row = int_of_string position_row_ in
+        let position_col = int_of_string position_col_ in
+        let velocity_row = int_of_string velocity_row_ in
+        let velocity_col = int_of_string velocity_col_ in
+      {
+        position_row ;
+        position_col ;
+        velocity_row ;
+        velocity_col ;
+      }
+    | _ -> {
+        position_row = -1 ;
+        position_col = -1 ;
+        velocity_row = -1 ;
+        velocity_col = -1 ;
+      }
+
+  let execute_move n {
+        position_row ;
+        position_col ;
+        velocity_row ;
+        velocity_col ;
+      } =
+    let new_position_row = (position_row + velocity_row * n) mod n_rows in
+    let new_position_col = (position_col + velocity_col * n) mod n_cols in
+    ((if new_position_row < 0 then new_position_row + n_rows else new_position_row),
+     (if new_position_col < 0 then new_position_col + n_cols else new_position_col))
+
+end
+
+let () =
+  let input_file = "inputs/day14.txt" in
+  let final_positions =
+  input_file
+  |> read_lines
+  |> List.filter (fun x -> String.length x > 0)
+  |> List.map Robot.create
+  |> List.map (Robot.execute_move 100)
+  in
+  let row_boundary = n_rows / 2 in
+  let col_boundary = n_cols / 2 in
+  let q1 =
+    List.filter (fun (row, col) -> row < row_boundary && col < col_boundary) final_positions
+    |> List.length in
+  let q2 =
+    List.filter (fun (row, col) -> row > row_boundary && col < col_boundary) final_positions
+    |> List.length in
+  let q3 =
+    List.filter (fun (row, col) -> row > row_boundary && col > col_boundary) final_positions
+    |> List.length in
+  let q4 =
+    List.filter (fun (row, col) -> row < row_boundary && col > col_boundary) final_positions
+    |> List.length in
+  q1 * q2 * q3 * q4 |> string_of_int |> print_endline
+
