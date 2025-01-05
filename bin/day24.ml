f1e007d8c478e95393dbf5a4c106e89b9e3e31d8
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2025-01-05 21:54:01 +0530

    Add partial code for day 24 part 2

--- bin/day24.ml
+++ bin/day24.ml
@@ -1,6 +1,22 @@
 open Utils
 
 let final_values = Hashtbl.create 16
+let final_values_2 = Hashtbl.create 16
+let relation_map = Hashtbl.create 16
+
+(* let get_relationships s = *)
+(*   let rec get_relationships_helper triplet = *)
+(*     let left, op, right = triplet in *)
+(*     if Hashtbl.mem final_values_2 left && Hashtbl.mem final_values_2 right then *)
+(*       "(" ^ left ^ " " ^ op ^ " " ^ right ^ ")" *)
+(*     else if (Hashtbl.mem final_values_2 left) then *)
+(*       "(" ^ left ^ " " ^ op ^ " " ^ (get_relationships_helper (Hashtbl.find relation_map right)) ^ ")" *)
+(*     else if (Hashtbl.mem final_values_2 right) then *)
+(*       "(" ^ (get_relationships_helper (Hashtbl.find relation_map left)) ^ " " ^ op ^ " " ^ right ^ ")" *)
+(*     else *)
+(*       "(" ^ (get_relationships_helper (Hashtbl.find relation_map left)) ^ " " ^ op ^ " " ^ (get_relationships_helper (Hashtbl.find relation_map right)) ^ ")" *)
+(*   in *)
+(*   get_relationships_helper (Hashtbl.find relation_map s) *)
 
 let () =
   let input_file = "inputs/day24.txt" in
@@ -12,7 +28,8 @@ let () =
       match x with
       | [a;b] ->
         let bool_val = String.trim b = "1" in
-        Hashtbl.add final_values a bool_val
+        (Hashtbl.add final_values a bool_val;
+         Hashtbl.add final_values_2 a bool_val)
       | _ -> ()
     );
   let original_relations =
@@ -59,3 +76,12 @@ let () =
       s := (if y then "1" else "0") ^ !s);
   print_int (int_of_string ("0b" ^ !s));
   print_endline "";
+  input_file
+  |> read_lines
+  |> List.filter (fun x -> String.contains x '-')
+  |> List.map (String.split_on_char ' ')
+  |> List.iter (fun x ->
+      match x with
+      | [l;op;r;_;res] ->
+        Hashtbl.add relation_map res (l, op, r);
+      | _ -> ());
